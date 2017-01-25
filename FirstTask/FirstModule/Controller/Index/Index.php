ea9a865ce2625c379787daccf3c10ec903a7b8c7
Ilya Novikov <ilya.novikov@itechart-group.com>
2017-01-26 01:47:06 +0300

    Basic module example

---
+++ FirstTask/FirstModule/Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Itechart\FirstModule\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        echo 'You did it! Congratulations!';
+    }
+}
